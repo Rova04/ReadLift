918c1a5bc32df256af765301ae0b37c0f087213e
Rova04 <161726355+Rova04@users.noreply.github.com>
2025-08-25 23:15:50 +0300

    Update firebase.js

--- front/firebase.js
+++ front/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzlRffm-PElgQ8EU5nkwl7BvoWXj60dPg",
+  apiKey: "Secretkey",
   authDomain: "lecteur-livre-da45b.firebaseapp.com",
   projectId: "lecteur-livre-da45b",
   storageBucket: "lecteur-livre-da45b.firebasestorage.app",
